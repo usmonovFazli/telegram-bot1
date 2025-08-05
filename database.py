685179df7f4466e71fe5b558fb81887e6e621da8
Fazliddin <mrfazli1221@gmail.com>
2025-08-05 09:34:23 +0500

    h

--- database.py
+++ database.py
@@ -4,16 +4,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_PARAMS = {
-    "dbname": os.getenv("PG_DB", "targibot1"),
-    "user": os.getenv("PG_USER", "targibot01"),
-    "password": os.getenv("PG_PASSWORD", "1psS0aV70pm6VRVk5TAfmDpqf4VTatJj"),
-    "host": os.getenv("PG_HOST", "dpg-d25oh8vdiees73c53oig-a"),
-    "port": os.getenv("PG_PORT", "5432"),
+    "dbname": os.getenv("PG_DB"),
+    "user": os.getenv("PG_USER"),
+    "password": os.getenv("PG_PASSWORD"),
+    "host": os.getenv("PG_HOST"),
+    "port": os.getenv("PG_PORT"),
 }
 
 
 def connect():
     return psycopg2.connect(**DB_PARAMS)
+
 
 def init_db():
     with connect() as conn:
@@ -31,6 +32,7 @@ def init_db():
             """)
         conn.commit()
 
+
 def add_or_update_channel(chat_id, title, members, chat_type="unknown", link=""):
     with connect() as conn:
         with conn.cursor() as cur:
@@ -45,6 +47,37 @@ def add_or_update_channel(chat_id, title, members, chat_type="unknown", link="")
             """, (chat_id, title, members, chat_type, link))
         conn.commit()
 
+
+def update_channel_status(chat_id, title=None, members=None, chat_type=None, link=None):
+    with connect() as conn:
+        with conn.cursor() as cur:
+            updates = []
+            values = []
+
+            if title is not None:
+                updates.append("title = %s")
+                values.append(title)
+            if members is not None:
+                updates.append("members = %s")
+                values.append(members)
+            if chat_type is not None:
+                updates.append("type = %s")
+                values.append(chat_type)
+            if link is not None:
+                updates.append("link = %s")
+                values.append(link)
+
+            if not updates:
+                return
+
+            values.append(chat_id)
+            query = f"""
+                UPDATE channels SET {', '.join(updates)} WHERE id = %s;
+            """
+            cur.execute(query, values)
+        conn.commit()
+
+
 def increment_video_count(chat_id):
     with connect() as conn:
         with conn.cursor() as cur:
@@ -52,6 +85,7 @@ def increment_video_count(chat_id):
                 UPDATE channels SET videos = videos + 1 WHERE id = %s;
             """, (chat_id,))
         conn.commit()
+
 
 def get_channels():
     with connect() as conn:
@@ -61,6 +95,3 @@ def get_channels():
                 FROM channels ORDER BY title;
             """)
             return cur.fetchall()
-
-def get_channels_full():
-    return get_channels()
